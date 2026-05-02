102e41640332438ef2936bfbf62430bdd700ab64
Nguyen Cong Dai <ndai200607@gmail.com>
2026-05-02 22:10:14 +0700

    Khoi tao Bai03 va them GitHub Actions

---
+++ src/BankException.java
@@ -0,0 +1,14 @@
+/**
+ * Ngoại lệ chung trong hệ thống ngân hàng.
+ */
+public class BankException extends Exception {
+
+  /**
+   * Khởi tạo ngoại lệ với thông báo lỗi.
+   *
+   * @param message thông báo lỗi chi tiết.
+   */
+  public BankException(String message) {
+    super(message);
+  }
+}
---
+++ src/Main.java
@@ -0,0 +1,9 @@
+/**
+ * Lớp thực thi chương trình chính.
+ */
+public class Main {
+  // Con giữ nguyên code cũ ở bên trong của con nhé
+  public static void main(String[] args) {
+    // ...
+  }
+}
