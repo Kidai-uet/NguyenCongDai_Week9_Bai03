cd0408fabd3e069f4d7f47c42f202bdec2f3c9f4
Nguyen Cong Dai <ndai200607@gmail.com>
2026-05-03 07:05:08 +0700

    ..

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,7 +4,6 @@ package main.java;
  * Lớp thực thi chương trình chính.
  */
 public class Main {
-  // Con giữ nguyên code cũ ở bên trong của con nhé
   public static void main(String[] args) {
     // ...
   }
